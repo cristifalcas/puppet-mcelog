b3d53887d4cc4905ca67a102f569d64c688b346d
root <root@is-albus.synygy.net>
2016-11-09 08:37:07 +0200

    fixes

--- manifests/config.pp
+++ manifests/config.pp
@@ -13,8 +13,14 @@ class mcelog::config {
     mode    => '0644',
     content => template("${module_name}/mcelog.conf.erb"),
   }
-  
-  if $mcelog::delete_mcelog_setup {
-    file { '/etc/mcelog/mcelog.setup': ensure => 'absent', }
+
+  if $::operatingsystemmajrelease == 7 and $mcelog::delete_mcelog_setup {
+    file { '/etc/mcelog/mcelog.setup':
+      ensure  => $file_ensure,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0755',
+      content => "#!/bin/sh\n#Managed by puppet\nexit 0\n",
+    }
   }
 }
